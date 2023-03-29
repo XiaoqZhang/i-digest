e6287504f3b5df2da775313aeea8aa363d0a5e1c
XiaoqZhang <xiaoqi.zhang@epfl.ch>
2023-03-29 19:08:24 +0200

    update pubchem

--- src/get_compound.py
+++ src/get_compound.py
@@ -1,6 +1,9 @@
 import pubchempy as pcp
 from pubchempy import get_compounds
+from rdkit import Chem
 
 for compound in get_compounds('glucose', 'name'):
-    c = pcp.Compound.from_cid(5090)
+    cid = compound.cid
+    c = pcp.Compound.from_cid(cid)
     print(c.isomeric_smiles)
+    Chem.MolFromSmiles(c.isomeric_smiles)
