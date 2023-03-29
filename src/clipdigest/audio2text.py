9e60974b23319feb6891cbe8d5a57120291df8aa
XiaoqZhang <xiaoqi.zhang@epfl.ch>
2023-03-29 21:34:52 +0200

    basic program structure

---
+++ src/clipdigest/audio2text.py
@@ -0,0 +1,50 @@
+import os
+import time
+import openai
+import whisper
+from loguru import logger
+
+def a2t(audio_path):
+    openai.api_key = os.environ['OPENAI_API_KEY']
+
+    model = whisper.load_model('base')
+    result = model.transcribe(audio_path, fp16=False)['text']
+
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+
+    message = "Give a summary of '%s'" %result
+
+    while message:
+        messages.append(
+            {
+                "role": "user",
+                "content": message
+            }
+        )
+        chat_completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        answer = chat_completion.choices[0].message.content
+        logger.info(f"Summary: {answer}")
+
+        message = "Come up with questions for '%s'" %result
+        messages.append(
+            {
+                "role": "user",
+                "content": message
+            }
+        )
+
+        chat_completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        answer = chat_completion.choices[0].message.content
+        logger.info(f"Questions: \n {answer}")
+
+        message = False
+
+    return result
--- src/get_compound.py
+++
@@ -1,9 +0,0 @@
-import pubchempy as pcp
-from pubchempy import get_compounds
-from rdkit import Chem
-
-for compound in get_compounds('glucose', 'name'):
-    cid = compound.cid
-    c = pcp.Compound.from_cid(cid)
-    print(c.isomeric_smiles)
-    Chem.MolFromSmiles(c.isomeric_smiles)
